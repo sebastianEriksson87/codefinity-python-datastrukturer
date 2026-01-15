6b8d50d5075f98c729cb685aa5bd3ed1ea4108cd
sebastianEriksson87 <>
2026-01-15 06:42:22 +0000

    Tuple Basics

--- beh_rska_python_tuple/tuple_basics/main.py
+++ beh_rska_python_tuple/tuple_basics/main.py
@@ -1,5 +1,7 @@
 # Write your code here
-space_movies = ___
+space_movies = ('2001: A Space Odyssey',
+'Interstellar', 'Star Wars: Episode IV - A New Hope',
+'Gravity', 'The Martian')
 
 # Testing
 print("Movies about space:", space_movies)
